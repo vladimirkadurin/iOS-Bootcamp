d3f7ee9ed2b702f86257d8cc6c3422f82b749d4b
Vladimir Kadurin <vladimir.kadurin@sumup.com>
2019-01-24 16:06:56 +0200

    Initial Commit

---
+++ SomeUp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SomeUp
+//
+//  Created by Vladimir Kadurin on 24.01.19.
+//  Copyright © 2019 Vladimir Kadurin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
